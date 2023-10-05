b64e7b3ce7dc49bd13c369aebd32fc5036b1fca2
dgamarra <dgamarra@continnetal.edu.pe>
2023-10-05 18:07:38 -0500

    Se añadió MCD

--- src/OperacionesAritmeticas.py
+++ src/OperacionesAritmeticas.py
@@ -1,3 +1,11 @@
 class OperacionesAritmeticas:
     def suma(self, sumando1, sumando2):
         return sumando1 + sumando2
+
+    def MCD(self, numero1, numero2):
+        temporal = 0
+        while numero2 != 0:
+            temporal = numero2
+            numero2 = numero1 % numero2
+            numero1 = temporal
+        return numero1
